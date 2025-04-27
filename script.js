805ea5d8d890cfea140ddadd286d88796b4227d9
Luxdoma <haltravelk@gmail.com>
2025-04-28 03:56:29 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
+// Переключение темы
 const toggleBtn = document.getElementById('theme-toggle');
 
-// Проверка на наличие кнопки и установка сохраненной темы при загрузке
-if (toggleBtn) {
-    const currentTheme = localStorage.getItem('theme') || 'light';
-    
-    // Если в localStorage сохранена тёмная тема, активируем её
-    if (currentTheme === 'dark') {
-        document.body.classList.add('dark');
-    }
-
-    // Обработчик клика по кнопке
-    toggleBtn.addEventListener('click', () => {
-        document.body.classList.toggle('dark');
-        
-        // Сохранение выбранной темы в localStorage
-        const newTheme = document.body.classList.contains('dark') ? 'dark' : 'light';
-        localStorage.setItem('theme', newTheme);
-    });
+// Проверка сохраненной темы из localStorage
+if (localStorage.getItem('theme') === 'dark') {
+    document.body.classList.add('dark');
 }
+
+toggleBtn.addEventListener('click', () => {
+    // Переключаем класс темной темы
+    document.body.classList.toggle('dark');
+
+    // Сохраняем выбранную тему в localStorage
+    if (document.body.classList.contains('dark')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
+    }
+});
