1d42c73415a642f7ce5fc9168053aaf201b2aed0
Luxdoma <haltravelk@gmail.com>
2025-04-28 04:55:59 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
+const icon = themeToggle.querySelector('i');
+
+// Начальное состояние
+if (!body.classList.contains('dark-mode')) {
+    body.classList.add('light-mode');
+}
 
 themeToggle.addEventListener('click', () => {
     body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
 
-    // Меняем иконку (луна ↔ солнце)
-    const icon = themeToggle.querySelector('i');
     if (body.classList.contains('dark-mode')) {
-        icon.classList.remove('fa-moon');
-        icon.classList.add('fa-sun');
-    } else {
         icon.classList.remove('fa-sun');
         icon.classList.add('fa-moon');
+    } else {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
     }
 });
