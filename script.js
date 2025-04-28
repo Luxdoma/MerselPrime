51df4e73013010028b9abd60f88d0ababf1a5fb0
Luxdoma <haltravelk@gmail.com>
2025-04-28 05:00:55 +0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ const themeToggle = document.getElementById('theme-toggle');
 const body = document.body;
 const icon = themeToggle.querySelector('i');
 
-// Начальное состояние
+// Инициализация
 if (!body.classList.contains('dark-mode')) {
     body.classList.add('light-mode');
+    icon.classList.add('fa-sun');
 }
 
 themeToggle.addEventListener('click', () => {
